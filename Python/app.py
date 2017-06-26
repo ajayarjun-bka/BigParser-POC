f98c301f8771ba507584917b8b310af29925530c
Ajay Arjun <arjun.bka@gmail.com>
2017-06-26 12:31:34 -0400

    Update app.py

--- Python/app.py
+++ Python/app.py
@@ -1,12 +1,15 @@
+# Import the module into the main app
 from module import BpData
 
-'''Example program to use BpData module to login and Query a grid from BigParser'''
+# Example program to use BpData module to login and Query a grid from BigParser
 
+# Function Call to fetch header of the specified grid
 fetchedData = BpData.fetchHeader("arjun.bka@gmail.com", "AjayArjun", "57a33a99e4b019ed65d2b00d");
 print(fetchedData)
 
+# Function Call to fetch rows from the specified grid based on the filtering parameters
 fetchedData = BpData.fetchData("arjun.bka@gmail.com", "AjayArjun",
                                {'gridId': '57a33a99e4b019ed65d2b00d', 'rowCount': '50',
                                 'keywords': ['Wine Bar'],
-                                'selectColumnsStoreName': ['0', '3']});
+                                'selectColumnsStoreName': [0, 3]});
 print(fetchedData)
