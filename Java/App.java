8b71052826f17b95cb09a499700928960ab37d9d
Ajay Arjun <arjun.bka@gmail.com>
2017-06-26 12:58:48 -0400

    Update App.java

--- Java/App.java
+++ Java/App.java
@@ -10,8 +10,12 @@ public class App {
 
 	public static void main(String args[]) {
 		String result=null;
+		// Function Call to fetch rows from the specified grid based on the filtering parameters
+		System.out.println("Request 1");
 		result=BpData.fetchData("abc@xyz.com", "password","{\"gridId\":\"57a33a99e4b019ed65d2b00d\",\"keywords\": [\"Wine Bar\"],\"selectColumnsStoreName\": [0,3],\"rowCount\":\"3\"}");
 		System.out.println(result);
+		// Function Call to fetch header of the specified grid
+		System.out.println("Request 2");
 		result=BpData.fetchHeader("abc@xyz.com", "password","57a33a99e4b019ed65d2b00d");
 		System.out.println(result);
 	}
